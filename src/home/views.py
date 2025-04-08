728c9eb329e6100d1032f2a9af464521769b87d8
Daniel-codemaster <dchembcy@gmail.com>
2025-04-08 21:07:56 +0200

    openweather integration

--- src/home/views.py
+++ src/home/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import requests
 
 @login_required(login_url='/identity/login')
 def index_view(request):
+    weather_data = {}
+    city = "Bulawayo"
+    city = request.GET.get('city')
+    if not city:
+        city = "Bulawayo"
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=5305292cb9a1302dd53f917e0e5e2fba&units=metric'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        weather_data = {
+            'city': f'{data['name']}, {data['sys']['country']}',
+            'temperature': data['main']['temp'],
+            'humidity': data['main']['humidity'],
+            'pressure': data['main']['pressure'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon']
+        }
+    else:
+        weather_data = {'error': 'City not found.'}
     context = {
         'welcome': "Welcome",
-        'title': "Home"
+        'title': "Home",
+        'weather_data': weather_data
     }
     return render(request, "index.html", context)
 
@@ -28,7 +49,9 @@ def irrigation_shedule_pred_view(request):
         return render(request, 'irrigation.html', context)
        
     context = {
-        'page_title': "Irrigation schedule"
+        'page_title': "Irrigation schedule",
+        'title': "Fertiliser prediction",
+        'breadcrumb':"Breadcrumb"
     }
     
     return render(request, 'irrigation.html', context)
